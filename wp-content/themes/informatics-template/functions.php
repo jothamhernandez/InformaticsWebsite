63a6a2d793bff1962f0046405b7cea57971caf95
Patrick Noceja <patrick@PinWheel.lpkkyz2abjmujg4y4jdtif5ilf.ix.internal.cloudapp.net>
2019-09-22 23:13:09 +0800

    Online store

--- wp-content/themes/informatics-template/functions.php
+++ wp-content/themes/informatics-template/functions.php
@@ -257,3 +257,35 @@ add_action('init','register_shop_sub_menu');
 
 
 remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
+
+
+
+// add_filter( 'woocommerce_checkout_fields' , 'bbloomer_add_email_verification_field_checkout' );
+  
+// function bbloomer_add_email_verification_field_checkout( $fields ) {
+ 
+// $fields['billing']['billing_email']['class'] = array('form-row-first');
+ 
+// $fields['billing']['billing_em_ver'] = array(
+//     'label' => __('Email Address Verification', 'bbloomer'),
+//     'required' => true,
+//     'class' => array('form-row-last'),
+//     'clear' => true,
+//     'priority' => 999,
+// );
+ 
+// return $fields;
+// }
+ 
+// // ---------------------------------
+// // 3) Generate error message if field values are different
+ 
+// add_action('woocommerce_checkout_process', 'bbloomer_matching_email_addresses');
+ 
+// function bbloomer_matching_email_addresses() { 
+// $email1 = $_POST['billing_email'];
+// $email2 = $_POST['billing_em_ver'];
+// if ( $email2 !== $email1 ) {
+// wc_add_notice( __( 'Your email addresses do not match', 'bbloomer' ), 'error' );
+// }
+// }
